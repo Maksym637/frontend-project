12c876ea5689109f99237a8c247d5bcf7999f047
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-06-14 14:47:23 +0300

    commit last changes

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/home.css';
+import './styles/home_popup.css';
 
 export default function Home() {
     return (
         <div>
             <div className="description">
-                <i>Here you can reserve a specific audience from 1 to 5 hours</i>
+                <i>Site&nbsp;&nbsp;for&nbsp;&nbsp;auditoriums&nbsp;&nbsp;reservation</i>
             </div>
             <div>
                 <Link to="/auditorium">
@@ -18,6 +19,28 @@ export default function Home() {
                 <Link to="/book">
                     <button className="nav-button-center">Create Reservation</button>
                 </Link>
+            </div>
+            <div className="wrap">
+                <div className="title-left nav-area-ti">
+                    <a id="header" href="#popup"><i>help ?</i></a>
+                </div>
+            </div>
+            <div id="popup" className="popup">
+                <a href="#header" className="popup__area"></a>
+                <div className="popup__body">
+                    <div className="popup__content">
+                        <a href="#" className="popup__close">✕</a>
+                        <div className="popup__title">Description</div>
+                        <div>
+                            Simple application for reservation auditoriums on a specific time.
+                            <p>1.) Here you can reserve auditoriums for a specific date and time from 1 to 5 hours.</p>
+                            <p>2.) You have ability to edit and delete your reservation.</p>
+                            <p>3.) <a className="note">
+                                Remember
+                            </a> : you cannot book auditoriums that are already taken.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
